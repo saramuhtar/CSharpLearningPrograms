98120fe14ea98fce724fe399daaa7699e2c93e59
saramuhtar <sm368c@gmail.com>
2017-03-01 19:02:37 +0300

    CSharpAdditionProgram
    My first C Sharp addition program

---
+++ Addition/Addition/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int sum;
+
+            Console.Write("Enter the first number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            sum = x + y;
+
+            Console.WriteLine("The value of the sum is: {0} ", sum);
+
+        }
+    }
+}
