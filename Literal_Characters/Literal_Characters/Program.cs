a9fcb565d1a08b22d48b22eac6c3bb399d3fcc7c
saramuhtar <sm368c@gmail.com>
2017-03-02 17:35:04 +0300

    Literal_Characters
    The difference between int, long, uint, float, double and decimal with
    writing in the 2 types

---
+++ Literal_Characters/Literal_Characters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literal_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = -100;
+            uint myuVar = 1000;
+            long myLVar = 1000l;
+
+            float myFlVar = 5.9f;
+            double mydblVar = 543.85;
+            decimal mydecVar = 100.85m;
+
+
+            Console.WriteLine("{0} \n{1} \n{2}", myVar, myuVar, myLVar);
+            Console.WriteLine($"\n\n\n\n{myFlVar}, \n{mydblVar}, \n{mydecVar}");
+
+            Console.ReadKey();
+        }
+    }
+}
